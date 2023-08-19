f94368c8210c515fec06c6098685ad2697b90234
KexySerJ90 <exserj9011@gmail.com>
2023-08-19 10:26:18 +0300

    Menu

--- main.py
+++ main.py
@@ -2,14 +2,30 @@ from tkinter import *
 
 window = Tk()
 window.title('Password manager')
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
-tomato_img = PhotoImage(file='logo.png')
-canvas.create_image(100, 100, image=tomato_img)
-canvas.pack()
+logo_img = PhotoImage(file='logo.png')
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=1, row=0)
 
+Website_label = Label(text="Website:")
+Website_label.grid(column=0, row=1)
+Email_label = Label(text="Email/Username:")
+Email_label.grid(column=0, row=2)
+Password_label = Label(text="Password:")
+Password_label.grid(column=0, row=3)
 
+entry_web = Entry(width=35)
+entry_web.grid(column=1, row=1, columnspan=2)
+entry_mail = Entry(width=35)
+entry_mail.grid(column=1, row=2, columnspan=2)
+entry_pas = Entry(width=19)
+entry_pas.grid(column=1, row=3)
+Generate_button = Button(text=" Generate Password", width=12)
+Generate_button.grid(column=2, row=3)
+Add_button = Button(text="Add", width=36)
+Add_button.grid(column=1, row=4, columnspan=2)
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
