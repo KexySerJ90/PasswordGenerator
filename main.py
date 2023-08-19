7c285ad35ab67cbcec42108de3ccdc38acc81815
KexySerJ90 <exserj9011@gmail.com>
2023-08-19 14:03:00 +0300

    Menu

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ window = Tk()
 window.title('Password manager')
 window.config(padx=50, pady=50)
 
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    with open('password.txt', mode='a') as f:
+        f.write()
+# ---------------------------- UI SETUP ------------------------------- #
+
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=logo_img)
@@ -18,22 +28,16 @@ Password_label.grid(column=0, row=3)
 
 entry_web = Entry(width=35)
 entry_web.grid(column=1, row=1, columnspan=2)
+entry_web.focus()
 entry_mail = Entry(width=35)
 entry_mail.grid(column=1, row=2, columnspan=2)
+entry_mail.insert(0, '@mail.ru')
 entry_pas = Entry(width=19)
 entry_pas.grid(column=1, row=3)
 Generate_button = Button(text=" Generate Password", width=12)
 Generate_button.grid(column=2, row=3)
-Add_button = Button(text="Add", width=36)
+Add_button = Button(text="Add", width=36, command=save)
 Add_button.grid(column=1, row=4, columnspan=2)
-
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
-# ---------------------------- SAVE PASSWORD ------------------------------- #
-
-# ---------------------------- UI SETUP ------------------------------- #
-
-
 
 
 window.mainloop()
